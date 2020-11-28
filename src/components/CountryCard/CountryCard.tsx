d141bc6c4f56eb863a80f0dc2dd26baeab97511b
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-11-27 21:54:24 -0300

    CountryCard inicio

---
+++ src/components/CountryCard/CountryCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const CountryCard = () => (
+  <p>Oi Card</p>
+)
+
+export default CountryCard
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import { Container } from '@material-ui/core'
 import Wrapper from 'components/WrapperContent'
+import { Grid } from '@material-ui/core'
+import CountryCard from 'components/CountryCard'
 
 const Home = () => {
   return (
     <Wrapper>
-      <p>Teste</p>
+      <div> Linha do Filtro </div>
+      <Grid container>
+        {[1,2,3,4,5,6,7].map(item => (
+          <Grid item xs={3} style={{ padding: '8px' }}>
+            <CountryCard />
+          </Grid>
+        ))}
+      </Grid>
     </Wrapper>
   )
 }
