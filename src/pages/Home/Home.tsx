4ae21fd0c30ea67b9abbbc359956c0b13b1a556f
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-11-27 23:18:25 -0300

    Inicio da implementação com maps

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Wrapper from 'components/WrapperContent'
 import { Grid } from '@material-ui/core'
 import CountryCard from 'components/CountryCard'
@@ -8,12 +8,21 @@ import CountryInfoQuery, { CountryInfoQueryResponse } from 'config/queries/count
 
 const Home = () => {
   const { data } = useQuery<CountryInfoQueryResponse>(CountryInfoQuery())
-  
+  const [mapInfo, setMapInfo] = useState<Map<string, CountryInfo>>(new Map())
+  useEffect(() => {
+    data?.Country.map((item: CountryInfo) => {
+      if (!mapInfo.get(item.nativeName)) {
+        return mapInfo.set(item.nativeName, item)
+      }
+    })
+    setMapInfo(value => value)
+  }, [data])
+
   return (
     <Wrapper>
       <div> Linha do Filtro </div>
       <Grid container>
-        {data?.Country.map((item: CountryInfo) => (
+        {Array.from(mapInfo.values()).map((item: CountryInfo) => (
           <Grid item xs={3} style={{ padding: '8px' }}>
             <CountryCard countryInfo={item} />
           </Grid>
