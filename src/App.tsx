003c61e623a5bdb235187b1d20f1291b6bf46d4b
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-11-27 23:05:41 -0300

    Adiciona provider apollo

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,23 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import Main from 'components/Main'
 import Header from 'components/Header'
+import { ApolloProvider } from '@apollo/client';
 import Routes from './routes'
 import store from './store'
 
 import './App.css'
+import client from 'config/gql';
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <Main>
-        <Header />
-        <Routes />
-      </Main>
-    </Provider>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <Main>
+          <Header />
+          <Routes />
+        </Main>
+      </Provider>
+    </ApolloProvider>
   )
 }
 
