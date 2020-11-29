7695b688c8da6b318e6f8692f7827372c52f632b
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-11-29 16:13:30 -0300

    cria reducer para adicionar infoCountry baseado em maps para facilitar edições

---
+++ src/store/modules/infoCountries/reducer.ts
@@ -0,0 +1,17 @@
+import CountryInfo from "shared/types/CountryInfo"
+import { action, actions } from './types'
+
+const infoMapState = new Map<string, CountryInfo>() 
+
+const infoCountries = (state: CountryInfo[] = [], action: action) => {
+  switch(action.type) {
+    case actions.add: {
+      infoMapState.set(action.countryInfo.nativeName, action.countryInfo)
+      state = Array.from(infoMapState.values())
+      return state
+    }
+    default: return state
+  }
+}
+
+export default infoCountries
--- src/store/modules/rootReducer.ts
+++ src/store/modules/rootReducer.ts
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux'
+import InfoReducer from './infoCountries/reducer'
 
 export default combineReducers({
-    
+  InfoReducer
 })
