0b469c1c3586987f3e7bc6d1ce2d554357a5d472
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-11-29 21:13:56 -0300

    Cria rota details

---
+++ src/pages/Details/Details.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Details: React.FC = () => {
+  return (
+    <h1>Oi Details</h1>
+  )
+}
+
+export default Details
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import { Switch, Route, BrowserRouter } from 'react-router-dom'
+import { Switch, Route, BrowserRouter, Redirect } from 'react-router-dom'
 
 import Home from 'pages/Home'
+import Details from 'pages/Details'
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route path='' exact component={Home} />
+      <Route path='/' exact component={Home} />
+      <Route path='/details'>
+        <Redirect to='' />
+      </Route>
+      <Route path='/details/:nativeName' component={Details} />
     </Switch>
   </BrowserRouter>
 )
