a46af792e8381b95420e59e2d989411ed3910227
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-11-30 00:18:04 -0300

    implementa details

---
+++ src/pages/Details/Details.style.ts
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+export const WrapperInfo = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+export const WrapperImage = styled.div`
+  display: flex;
+  align-items: center;
+  width: 300px;
+  img {
+    width: 64px;
+    margin-right: 16px;
+  }
+`
+
+export const WrapperMain = styled.div`
+  width: 300px;
+  p {
+    color: gray;
+    > span {
+      color: black;
+    }
+    > ul {
+      display: flex;
+      flex-direction: column;
+      color: black;
+      align-items: center;
+    }
+  }
+`
--- src/pages/Details/Details.tsx
+++ src/pages/Details/Details.tsx
@@ -1,9 +1,94 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Divider, Grid, Typography } from '@material-ui/core'
+import { connect, ConnectedProps } from 'react-redux'
+import { infoState } from 'store/modules/infoCountries'
+import WrapperContent from 'components/WrapperContent'
+import Country from 'shared/types/Country'
+import { WrapperInfo, WrapperImage, WrapperMain } from './Details.style'
 
-const Details: React.FC = () => {
-  return (
-    <h1>Oi Details</h1>
-  )
+const mapStateToProps = (state: infoState) => {
+  return ({
+    infosMap: state.InfoReducer.mapState
+  })
 }
 
-export default Details
+const dispatchProps = {
+}
+
+const connector = connect(mapStateToProps, dispatchProps)
+
+type PropsFromRedux = ConnectedProps<typeof connector>
+
+type Props = PropsFromRedux & {
+  match: {
+    params: {
+      nativeName: string
+    }
+  }
+}
+
+const Details: React.FC<Props> = ({ match: { params: { nativeName }}, infosMap }) => {
+  const [countryInfo, setCountryInfo] = useState<Country>()
+
+  useEffect(() => {
+    const searchItem = infosMap.get(nativeName)
+    if(searchItem) {
+      setCountryInfo(searchItem)
+    } else {
+      window.location.href = '/'
+    }
+  }, [])
+
+  if(countryInfo) {
+    return (
+      <WrapperContent>
+        <Grid container style={{ height: '100%' }}>
+          <WrapperInfo>
+            <WrapperImage>
+              <img src={countryInfo.flag.svgFile} alt={`Bandeira ${countryInfo.name}`}/>
+              <Typography variant="h3">
+                {countryInfo.name}
+              </Typography>
+            </WrapperImage>
+            <WrapperMain>
+              <Typography variant="subtitle1">
+                {countryInfo.nativeName}
+              </Typography>
+
+              <Divider />
+
+              <div>
+                <Typography variant="body2" component="p">
+                  Capital: <Typography variant="body1" component="span">{countryInfo.capital}</Typography>
+                </Typography>
+
+                <Typography variant="body2" component="p">
+                  Área: <Typography variant="body1" component="span">{countryInfo.area}</Typography>
+                </Typography>
+
+                <Typography variant="body2" component="p">
+                  População: <Typography variant="body1" component="span">{countryInfo.population}</Typography>
+                </Typography>
+
+                <Typography variant="body2" component="p">
+                  TLD (Top-Level Domain): <ul>
+                  {
+                    countryInfo.topLevelDomains.map(item => {
+                      return (
+                        <li>{item.name}</li>
+                      )
+                    })
+                  }
+                  </ul>
+                </Typography>
+              </div>
+            </WrapperMain>
+          </WrapperInfo>
+        </Grid>
+      </WrapperContent>
+    )
+  }
+  return <h1>Não há registros</h1>
+}
+
+export default connector(Details)
